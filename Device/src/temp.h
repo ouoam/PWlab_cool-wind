e7e622524a4fafd76f6ce97dca35572a399a852c
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-07-16 23:39:42 +0700

    Add temp sensor

--- Device/src/temp.h
+++ Device/src/temp.h
@@ -6,38 +6,52 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define SENSOR_PRE_TEMP_PIN 2
+#define SENSOR_EX_TEMP_PIN  3
+
 #define MAX_HISTORY 10
 
 class Temp
 {
 private:
-    OneWire oneWire = OneWire(2); // PORT For ds18b20 - upto 1/16
-    DallasTemperature sensors = DallasTemperature(&oneWire);
+    OneWire oneWirePre = OneWire(SENSOR_PRE_TEMP_PIN); // PORT For ds18b20 - upto 1/16
+    OneWire oneWireEx  = OneWire(SENSOR_EX_TEMP_PIN);
+    DallasTemperature sensorsPre = DallasTemperature(&oneWirePre);
+    DallasTemperature sensorsEx  = DallasTemperature(&oneWireEx);
+
     float tempHistory[MAX_HISTORY];
+    float lastExTemp;
     uint8_t i_tempHistory = 0;
     uint64_t nextGetTemp = 0;
 
 public:
     Temp()
     {
-        sensors.setWaitForConversion(false);
-        sensors.requestTemperatures();
+        sensorsPre.setWaitForConversion(false);
+        sensorsEx.setWaitForConversion(false);
+        sensorsPre.requestTemperatures();
+        sensorsEx.requestTemperatures();
         nextGetTemp = millis() + 750;
     }
 
     void setup()
     {
-        sensors.begin();
+        sensorsPre.begin();
+        sensorsEx.begin();
     }
 
     void loop()
     {
         if (millis() > nextGetTemp)
         {
-            tempHistory[i_tempHistory] = sensors.getTempCByIndex(0);
+            tempHistory[i_tempHistory] = sensorsPre.getTempCByIndex(0);
             i_tempHistory++;
             i_tempHistory %= MAX_HISTORY;
-            sensors.requestTemperatures();
+            sensorsPre.requestTemperatures();
+
+            lastExTemp = sensorsEx.getTempCByIndex(0);
+            sensorsEx.requestTemperatures();
+
             nextGetTemp = millis() + 750;
         }
     }
@@ -47,6 +61,11 @@ public:
         return tempHistory[(i_tempHistory + MAX_HISTORY - 1) % MAX_HISTORY];
     }
 
+    float getLastExTemp()
+    {
+        return lastExTemp;
+    }
+
     int32_t getEstimateTimeTo(int8_t pretemp)
     {
         int time = 750 * MAX_HISTORY;
@@ -54,12 +73,6 @@ public:
         double changePerTime = diff / time;
         float nowToPre = getLastTemp() - pretemp;
         return nowToPre / changePerTime;
-    }
-
-    void printTemp()
-    {
-        // Serial.print("Temp C: ");
-        // Serial.println(getLastTemp(), 4);
     }
 };
 
