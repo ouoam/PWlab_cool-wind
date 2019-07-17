c6005af5b17d88d1dbc353b5adf93349de6b4106
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-07-17 23:05:27 +0700

    Add defrost and pause feature

--- Device/src/display.h
+++ Device/src/display.h
@@ -13,7 +13,8 @@ uint8_t fanspeed;
 unsigned long valRemain = 0;
 unsigned long startTime = 0;
 unsigned long nextChangeTime = 0;
-bool forceReload = false;
+bool isPauseVal = false;
+bool isDeforceVal = false;
 
 NexPage settingPg = NexPage(0, 0, "main");
 NexPage workingPg = NexPage(2, 0, "working");
@@ -36,7 +37,9 @@ void startBtnCallBack(void *ptr)
     io.setBlow(true);
     startTime = millis();
     nextChangeTime = 0;
-    valRemain = 10ul * 60 * 1000;
+    uint32_t timeMin = 0;
+    while (!valTime.getValue(&timeMin));
+    valRemain = timeMin * 60 * 1000;
 }
 
 // ---------- page 2 ----------
@@ -48,13 +51,18 @@ NexHotspot timeMinus = NexHotspot(2, 7, "timeMinus");
 NexHotspot timePlus = NexHotspot(2, 8, "timePlus");
 NexHotspot fanMinus = NexHotspot(2, 9, "fanMinus");
 NexHotspot fanPlus = NexHotspot(2, 10, "fanPlus");
+NexHotspot hDefrost = NexHotspot(2, 11, "hDefrost");
+NexHotspot hPause = NexHotspot(2, 12, "hPause");
 NexHotspot hStop = NexHotspot(2, 13, "hStop");
+
+NexVariable isPause = NexVariable(2, 3, "isPause", "working");
+NexVariable isDefrost = NexVariable(2, 4, "isDefrost", "working");
 
 void timeMinusCallBack(void *ptr)
 {
     if (valRemain > 61ul * 1000) {
         valRemain -= 60ul * 1000;
-        forceReload = true;
+        nextChangeTime = 0;
     }
 }
 
@@ -62,7 +70,7 @@ void timePlusCallBack(void *ptr)
 {
     if (valRemain < 99ul * 60 * 1000) {
         valRemain += 60ul * 1000;
-        forceReload = true;
+        nextChangeTime = 0;
     }
 }
 
@@ -84,6 +92,51 @@ void fanPlusCallBack(void *ptr)
     io.setSpeed(fanspeed);
 }
 
+void hDefrostCallBack(void *ptr)
+{
+    isDeforceVal = !isDeforceVal;
+    isDefrost.setValue(isDeforceVal);
+    nextChangeTime = 0;
+
+    if (isDeforceVal) { // On deforce
+        io.setBlow(true);
+        fanspeed = 6;
+        fanSpeed.setValue(fanspeed);
+        io.setSpeed(fanspeed);
+        if (!isPauseVal) {
+            valRemain -= millis() - startTime;
+        }
+    } else { // Off deforce
+        if (!isPauseVal) {
+            io.setBlow(true);
+            startTime = millis();
+        } else {
+            io.setBlow(false);
+        }
+    }
+}
+
+void hPauseCallBack(void *ptr)
+{
+    isPauseVal = !isPauseVal;
+    isPause.setValue(isPauseVal);
+    nextChangeTime = 0;
+
+    if (isPauseVal) { // On pause
+        if (!isDeforceVal) {
+            io.setBlow(false);
+            valRemain -= millis() - startTime;
+        } else {
+            io.setBlow(true);
+        }
+    } else { // Off pause
+        if (!isDeforceVal) {
+            io.setBlow(true);
+            startTime = millis();
+        }
+    }
+}
+
 void hStopCallBack(void *ptr)
 {
     settingPg.show();
@@ -99,6 +152,8 @@ NexTouch *nex_listen_list[] =
         &timePlus,
         &fanMinus,
         &fanPlus,
+        &hDefrost,
+        &hPause,
         &hStop,
         NULL};
 
@@ -106,7 +161,6 @@ class Display
 {
 private:
     int8_t lastPreTemp = 0;
-    
 
 public:
     Display(/* args */)
@@ -131,7 +185,9 @@ public:
         fanMinus.attachPush(fanMinusCallBack, &fanMinus);
         fanPlus.attachPush(fanPlusCallBack, &fanPlus);
 
-        //   -----  pause stop  -----
+        //   -----  deforce pause stop  -----
+        hDefrost.attachPush(hDefrostCallBack, &hDefrost);
+        hPause.attachPush(hPauseCallBack, &hPause);
         hStop.attachPush(hStopCallBack, &hStop);
 
         page = 0;
@@ -142,8 +198,10 @@ public:
     {
         nexLoop(nex_listen_list);
         if (page == 2) {
-            if (millis() >= nextChangeTime || forceReload) {
-                long remain = (long)valRemain - (long)(millis() - startTime);
+            if (millis() >= nextChangeTime && ((!isPauseVal && !isDeforceVal) || nextChangeTime == 0)) {
+                unsigned long use = millis() - startTime;
+                if (isPauseVal || isDeforceVal) use = 0;
+                long remain = (long)valRemain - (long)use;
                 if (remain > 0) {
                     remain /= 1000;
                     String out = "";
@@ -152,12 +210,11 @@ public:
                     out += (remain % 60 >= 10) ? "" : "0";
                     out += remain % 60;
                     remainTime.setText(out.c_str());
-                    nextChangeTime = millis() + 1000;
 
-                    unsigned long use = millis() - startTime;
                     startTime += use;
                     valRemain -= use;
-                    forceReload = false;
+
+                    nextChangeTime = millis() + (valRemain % 1000);
                 } else {
                     settingPg.show();
                     page = 0;
